3655de4f4b64e8a9f3c7f0ccf962a6914a2a8594
Emily Chandler <emilychandler@Emilys-MacBook-Pro.local>
2022-09-27 21:39:41 -0600

    final project 2

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const mongodb = require('./db/connect');
-//const professionalRoutes = require('./routes/professional');
 
 const port = process.env.PORT || 8080;
 const app = express();
